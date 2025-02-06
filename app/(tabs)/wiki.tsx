9e653000a77d2972be36f49afa81e0dc7f2de026
commodore_64 <cesarml2002@hotmail.com>
2025-02-05 18:57:14 -0600

    IN-106 v1

--- app/(tabs)/wiki.tsx
+++ app/(tabs)/wiki.tsx
@@ -1,4 +1,3 @@
-// app/wiki.tsx
 import * as SplashScreen from "expo-splash-screen";
 import React, { useEffect } from "react";
 import { ImageBackground, StyleSheet, Text, View } from "react-native";
@@ -33,8 +32,32 @@ export default function WikiScreen() {
       imageStyle={{ ...styles.image, transform: [{ rotate: "180deg" }] }}
     >
       <View style={styles.container}>
-        <Text style={styles.text}>Wiki</Text>
+        <Text style={styles.title}>Wiki</Text>
         <View style={styles.separator} />
+        <View style={styles.content}>
+          <Text style={styles.header}>Personajes</Text>
+          <Text style={styles.subheader}>Principal:</Text>
+          <Text style={styles.text}>
+            El protagonista es un personaje curioso y determinado, cuya meta
+            principal es escapar de la mina y descubrir los misterios que la
+            rodean. Aunque no cuenta con armas ni habilidades extraordinarias,
+            su fortaleza radica en su ingenio y su capacidad de observación.
+          </Text>
+          <Text style={styles.subheader}>Antagonista:</Text>
+          <Text style={styles.text}>
+            Una entidad oscura y malévola que habita en las profundidades de la
+            mina. Se manifiesta de diferentes formas para acechar y obstaculizar
+            al protagonista. Su naturaleza enigmática añade una capa de terror
+            psicológico, manteniendo al jugador en constante tensión.
+          </Text>
+          <Text style={styles.subheader}>Orbe de fuego:</Text>
+          <Text style={styles.text}>
+            Este personaje actúa como un mentor al inicio del juego, guiando al
+            protagonista hacia una zona de interés. Representa una fuente de luz
+            y esperanza en un entorno dominado por la oscuridad. Sin embargo, su
+            propósito y naturaleza pueden ser más complejos de lo que aparentan.
+          </Text>
+        </View>
       </View>
     </ImageBackground>
   );
@@ -53,7 +76,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  text: {
+  title: {
     color: "#fff",
     fontSize: 38,
     fontFamily: "MyCustomFont",
@@ -72,5 +95,34 @@ const styles = StyleSheet.create({
     position: "absolute",
     top: 155,
     left: 30,
+  },
+  content: {
+    marginTop: 350,
+    paddingHorizontal: 20,
+    fontFamily: "MyCustomFont",
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+    color: "#fff",
+    fontFamily: "MyCustomFont",
+  },
+  subheader: {
+    fontSize: 20,
+    fontWeight: "600",
+    marginTop: 10,
+    color: "#87CEFA", // Light blue color
+    textShadowColor: "#B0E0E6", // Light blue shadow for glow effect
+    textShadowOffset: { width: 0, height: 0 },
+    textShadowRadius: 10,
+    fontFamily: "MyCustomFont",
+  },
+  text: {
+    fontSize: 16,
+    color: "#fff",
+    marginTop: 5,
+    lineHeight: 22,
+    fontFamily: "MyCustomFont",
   },
 });
